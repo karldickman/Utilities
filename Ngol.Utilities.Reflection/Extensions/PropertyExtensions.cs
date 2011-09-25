28d0314212376bd47d308f54cfc2e3f1696cab8a
karldickman <karldickman@gmail.com>
2011-09-24 20:26:09 -0700

    Add GetProperty extensions method to the reflection utilities.

--- Ngol.Utilities.Reflection/Extensions/PropertyExtensions.cs
+++ Ngol.Utilities.Reflection/Extensions/PropertyExtensions.cs
@@ -18,6 +18,89 @@ namespace Ngol.Utilities.Reflection.Extensions
         /// <param name="propertyName">
         /// A <see cref="System.String"/>
         /// </param>
+        public static TPropertyType GetProperty<TInstance, TPropertyType>(this TInstance instance, string propertyName)
+        {
+            return (TPropertyType)GetProperty(instance, typeof(TInstance), propertyName);
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="instance">
+        /// A <see cref="System.Object"/>
+        /// </param>
+        /// <param name="propertyName">
+        /// A <see cref="System.String"/>
+        /// </param>
+        public static object GetProperty(this object instance, string propertyName)
+        {
+            return GetProperty(instance, instance.GetType(), propertyName);
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="instance">
+        /// A <see cref="System.Object"/>
+        /// </param>
+        /// <param name="type">
+        /// A <see cref="Type"/>
+        /// </param>
+        /// <param name="propertyName">
+        /// A <see cref="System.String"/>
+        /// </param>
+        public static object GetProperty(this object instance, Type type, string propertyName)
+        {
+            return GetProperty(instance, type, propertyName, CommonBindingFlags.InstanceFlags);
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="propertyName">
+        /// A <see cref="System.String"/>
+        /// </param>
+        public static TPropertyType GetProperty<TClass, TPropertyType>(string propertyName)
+        {
+            return (TPropertyType)GetProperty(typeof(TClass), propertyName);
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="type">
+        /// A <see cref="Type"/>
+        /// </param>
+        /// <param name="propertyName">
+        /// A <see cref="System.String"/>
+        /// </param>
+        public static TPropertyType GetProperty<TPropertyType>(this Type type, string propertyName)
+        {
+            return (TPropertyType)GetProperty(type, propertyName);
+        }
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="type">
+        /// A <see cref="Type"/>
+        /// </param>
+        /// <param name="propertyName">
+        /// A <see cref="System.String"/>
+        /// </param>
+        public static object GetProperty(this Type type, string propertyName)
+        {
+            return GetProperty(null, type, propertyName, CommonBindingFlags.StaticFlags);
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="instance">
+        ///
+        /// </param>
+        /// <param name="propertyName">
+        /// A <see cref="System.String"/>
+        /// </param>
         /// <param name="value">
         ///
         /// </param>
@@ -103,6 +186,33 @@ namespace Ngol.Utilities.Reflection.Extensions
         public static void SetProperty(this Type type, string propertyName, object value)
         {
             SetProperty(null, type, propertyName, value, CommonBindingFlags.StaticFlags);
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="instance">
+        /// A <see cref="System.Object"/>
+        /// </param>
+        /// <param name="type">
+        /// A <see cref="Type"/>
+        /// </param>
+        /// <param name="propertyName">
+        /// A <see cref="System.String"/>
+        /// </param>
+        /// <param name="bindingFlags">
+        /// A <see cref="BindingFlags"/>
+        /// </param>
+        private static object GetProperty(object instance, Type type, string propertyName, BindingFlags bindingFlags)
+        {
+            if(type == null)
+                throw new ArgumentNullException("type");
+            PropertyInfo propertyInfo = type.GetProperty(propertyName, bindingFlags);
+            if(propertyInfo == null)
+            {
+                throw new ArgumentException(string.Format("There is no property {0} on type {1}.", propertyName, type.Name));
+            }
+            return propertyInfo.GetValue(instance, null);
         }
 
         /// <summary>
